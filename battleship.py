ecf9845e4ad3f7d3fa9888aa7a6f4e39f79a7e95
PaVaN-02 <pavanpillalamarri222@msitprogram.net>
2021-12-17 09:51:36 +0530

    completed addship

--- battleship.py
+++ battleship.py
@@ -62,7 +62,14 @@ Parameters: int ; int
 Returns: 2D list of ints
 '''
 def emptyGrid(rows, cols):
-    return
+    row=[]
+    for i in range (rows):
+        col=[]
+        for j in range(cols):
+            col.append(1)
+        row.append(col)    
+
+    return row
 
 
 '''
@@ -71,8 +78,25 @@ Parameters: no parameters
 Returns: 2D list of ints
 '''
 def createShip():
-    return
+    import random
+    row = random.randint(1,8)
+    col = random.randint(1,8)
+    hv = random.randint(0,1)
+    if hv==0:
+      p=row-1
+      randomValue=col
+    else:
+      p=col-1
+      randomValue=row
+    d1=[[randomValue for j in range(1)] for i in range(3)]
 
+    for i in range(len(d1)):
+      d2=d1[i]
+ 
+      d2.insert(hv,p)
+      p=p+1
+    
+    return d1
 
 '''
 checkShip(grid, ship)
@@ -80,7 +104,16 @@ Parameters: 2D list of ints ; 2D list of ints
 Returns: bool
 '''
 def checkShip(grid, ship):
-    return
+    count=0
+    for i in range(3):
+        row=ship[i][0]
+        col=ship[i][1]
+        if grid[row][col]==1:
+           count=count+1
+    if count==3:
+        return True
+    else:
+       return False
 
 
 '''
