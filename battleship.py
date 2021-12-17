38cd2ca21d2cd00a5c026157b22a22679f7caefd
PaVaN-02 <pavanpillalamarri222@msitprogram.net>
2021-12-17 09:46:42 +0530

    completed checkship

--- battleship.py
+++ battleship.py
@@ -62,8 +62,14 @@ Parameters: int ; int
 Returns: 2D list of ints
 '''
 def emptyGrid(rows, cols):
-    return
+    row=[]
+    for i in range (rows):
+        col=[]
+        for j in range(cols):
+            col.append(1)
+        row.append(col)    
 
+    return row
 
 '''
 createShip()
@@ -71,7 +77,25 @@ Parameters: no parameters
 Returns: 2D list of ints
 '''
 def createShip():
-    return
+    import random
+    row = random.randint(1,8)
+    col = random.randint(1,8)
+    hv = random.randint(0,1)
+    if hv==0:
+      p=row-1
+      randomValue=col
+    else:
+      p=col-1
+      randomValue=row
+    d1=[[randomValue for j in range(1)] for i in range(3)]
+
+    for i in range(len(d1)):
+      d2=d1[i]
+ 
+      d2.insert(hv,p)
+      p=p+1
+    
+    return d1
 
 
 '''
